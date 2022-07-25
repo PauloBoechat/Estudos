506389e016ace404a486f699fa25dd1b22c24659
Paulo Fernando <paulovcd40@gmail.com>
2022-07-25 19:39:39 -0300

    kk

---
+++ python/Exercícios/Desafio 97.py
@@ -0,0 +1,11 @@
+
+
+def escreva(val):
+    val = str(val)
+    print('-'*(len(val)+2))
+    print(' '+val)
+    print('-'*(len(val)+2))
+
+Titulo = str(input("Digite o'que estará escrito no Título: "))
+
+escreva(Titulo)
