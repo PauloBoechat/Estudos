2b5b31804c3a29922c7a967cae508ab6012e1316
Paulo Fernando Boechat <paulovcd40@fedora>
2022-10-25 20:13:50 -0300

    Update exercicio 3.2.py and exercicio 3.6.py

---
+++ python/exercicios Livro python/exercicio 3.2.py
@@ -0,0 +1,27 @@
+a = 4 
+c = 5.0
+d = 1
+f = 5
+b = 10
+
+print(a == c)
+print('=-'*20)
+print(a < b)
+print('=-'*20)
+print(d > b)
+print('=-'*20)
+print(c != f)
+print('=-'*20)
+print(a == b)
+print('=-'*20)
+print(c < d)
+print('=-'*20)
+print(b > a)
+print('=-'*20)
+print(c >= f)
+print('=-'*20)
+print(f >= c)
+print('=-'*20)
+print(c <= c)
+print('=-'*20)
+print(c <= f)
