9898e61fbefd62951920d544f93c65bef3c9bb92
Paulo Fernando <101608101+PauloBoechat@users.noreply.github.com>
2024-01-09 09:53:45 -0300

    Atualização estudos C#

--- C#/Projeto1/Program.cs
+++ C#/Projeto1/Program.cs
@@ -1,21 +1,12 @@
 ﻿using Projeto1.Models;
 
-int quantidadeSys = 10;
+Calculadora calc = new Calculadora();
 
-Console.WriteLine("Sistema de Vendas E-Commerce");
-Console.Write("Digite a quantidade de produtos que você vai comprar: ");
-int quantidadeUser = Convert.ToInt32(Console.ReadLine());
-
-if (quantidadeUser == 0)
-{
-    Console.WriteLine("Você não comprou nada!");
-}
-else if (quantidadeUser <= quantidadeSys)
-{
-    Console.WriteLine("Compra efetuada com sucesso!");
-}
-else
-{
-    Console.WriteLine($"Desculpe, mas não temos mais do que {quantidadeSys} no nosso estoque.");
-}
-Console.ReadKey();
+//calc.Somar(2,5);
+//calc.Subtrair(10, 50);
+//calc.Multiplicar(15, 45);
+//calc.Dividir(10, 2);
+//calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
